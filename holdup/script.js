5d802b931b23a1943cab2ba79443c78ca11d7a26
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-10-28 18:03:30 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -1,6 +1,6 @@
 document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Address " +userip+")."
 
-setInterval(goToScreen, 1000);
+setInterval(goToScreen, 15000);
 
 goToScreen(){
   window.location.replace("https://supernova-site.pages.dev/welcome/#home") 
