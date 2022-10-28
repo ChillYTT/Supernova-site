e63d22ac8841e7010b7384bfff611a0b6c48ff25
Anon <chilll.website.supp0rt@gmail.com>
2022-10-28 09:50:44 -0400

    Create script.js

---
+++ holdup/script.js
@@ -0,0 +1,6 @@
+var ip = userip.toString();
+if(ip == "69.74.172.194"){
+document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Address " +userip+")."
+}else{
+  window.location.replace("supernova-site.pages.dev/welcome/index.html");
+}
