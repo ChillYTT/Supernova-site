a91f75c8d17ade8b2588b7fd2f1bf4ed2e967e48
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-10-28 17:59:49 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -1,6 +1,1 @@
-var ip = userip.toString();
-if(ip == "69.74.172.194"){
 document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Address " +userip+")."
-}else{
-window.location.replace("/welcome/index.html");
-}
