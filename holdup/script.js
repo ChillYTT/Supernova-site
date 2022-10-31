68ddc22fc1ba3132b82d93d6a23fcdb260258a69
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-10-31 10:25:37 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -1,25 +1,11 @@
 document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Address " +userip+")."
 
-var key1 = false;
-var key2 = false;
-var key3 = false;
-var key4 = false;
+
 window.onload = function() {
    document.getElementsByTagName('body')[0].onkeyup = function(e) { 
       var ev = e || event;
-      if(ev.keyCode == 72) {
-         key1 = true;
-      }
-     if(ev.keyCode == 69 && key1 == true){
-         key2 = true; 
-     }
-     if(ev.keyCode == 76 && key2 == true){
-         key3 = true; 
-     }
-     if(ev.keyCode == 76 && key3 == true){
-        key4 = true;
-     }
-     if(ev.keyCode == 69 && key4 == true){
+
+     if(ev.keyCode == 13){
        document.getElementById("iframestuff").src="supernova-site.pages.dev/holdup/index.html";
      }
    }
