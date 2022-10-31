8702d8f17f3865b013e7ab1a1a2d154c984b8fdb
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-10-31 10:22:32 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -1,7 +1,26 @@
 document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Address " +userip+")."
 
-setTimeout(goToScreen, 15000);
-
-goToScreen(){
-  window.location.replace("https://supernova-site.pages.dev/welcome/#home") 
-}
+var key1 = false;
+var key2 = false;
+var key3 = false;
+var key4 = false;
+window.onload = function() {
+   document.getElementsByTagName('body')[0].onkeyup = function(e) { 
+      var ev = e || event;
+      if(ev.keyCode == 72) {
+         key1 = true;
+      }
+     if(ev.keyCode == 69 && key1 == true){
+         key2 = true; 
+     }
+     if(ev.keyCode == 76 && key2 == true){
+         key3 = true; 
+     }
+     if(ev.keyCode == 76 && key3 == true){
+        key4 = true;
+     }
+     if(ev.keyCode == 69 && key4 == true){
+       document.getElementById("iframestuff").src="supernova-site.pages.dev/holdup/index.html";
+     }
+   }
+};
