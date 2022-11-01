6a683ee3e72213df1e6da4164e5ceceb7d626930
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-11-01 09:35:55 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -4,3 +4,44 @@ var ip = userip.toString();
 if(ip != "69.74.172.194"){
     window.location.replace("supernova-site.pages.dev/welcome/index.html")
 }
+
+ var started = true;
+  var key1 = false;
+   var key2 = false;
+   var key3 = false;
+   var key4 = false;
+var key5 = false;
+var key6 = false;
+document.addEventListener('keydown', (event) => {
+  var name = event.key;
+  var code = event.code;
+
+   if(event.keyCode == 73 && started){
+     key1 = true;
+     started = false;
+     alert("okay");
+   }
+   if(event.keyCode == 78 && key1){
+     key2 = true;
+     key1 = false;
+   }
+   if(event.keyCode == 86 && key2){
+     key3 = true;
+     key2 = false;
+   }
+   if(event.keyCode == 73 && key3){
+     key4 = true;
+     key3 = false;
+   }
+  if(event.keyCode == 84 && key4){
+    key4 = false;
+    key5 = true;
+  }
+  if(event.keyCode == 69 && key5){
+    key5 = false;
+    key6 = true;
+  }
+  if (name === 'Enter' && key6) {
+    document.getElementById("iframestuff").src="/welcome/index.html";
+  }
+}, false);
