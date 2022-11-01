df21a311c79e37d7171288af27ab1b8f343024ac
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-10-31 21:36:43 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -2,7 +2,7 @@ document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Addres
 
 var ip = userip.toString();
 if(ip != "69.74.172.194"){
-    document.getElementById("iframestuff").src="/welcome/index.html";
+    window.location.replace("supernova-site.pages.dev/welcome/index.html
 }
 
 document.body.addEventListener('keypress', function(e) {
