a6ea64ce93781b928783649e4c4ca223704affc3
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-10-31 21:42:47 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -2,15 +2,5 @@ document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Addres
 
 var ip = userip.toString();
 if(ip != "69.74.172.194"){
-    window.location.replace("supernova-site.pages.dev/welcome/index.html
+    window.location.replace("supernova-site.pages.dev/welcome/index.html")
 }
-
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  var code = event.code;
-  // Alert the key name and key code on keydown
-  if (name === 'Enter') {
-    window.location.replace("/welcome/index.html")
-  }
-
-}, false);
