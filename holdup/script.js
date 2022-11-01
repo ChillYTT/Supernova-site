37eafd68eaaa9b539ea766b2a7983fc06630f1bf
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-10-31 21:41:03 -0400

    Update script.js

--- holdup/script.js
+++ holdup/script.js
@@ -5,14 +5,12 @@ if(ip != "69.74.172.194"){
     window.location.replace("supernova-site.pages.dev/welcome/index.html
 }
 
-document.body.addEventListener('keypress', function(e) {
-    var e = document.createEvent('KeyboardEvent');
-     if (e.keyCode == 27) { 
-        window.location.replace("supernova-site.pages.dev/welcome/index.html");
-    }
-});
+document.addEventListener('keydown', (event) => {
+  var name = event.key;
+  var code = event.code;
+  // Alert the key name and key code on keydown
+  if (name === 'Enter') {
+    window.location.replace("/welcome/index.html")
+  }
 
-window.addEventListener("keydown", function (event) {
-
-  window.location.replace("supernova-site.pages.dev/welcome/index.html");
-});
+}, false);
