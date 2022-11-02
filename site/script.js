0a8c442edcee53ee219fd9136dcff6bbd6cc05e0
ben10101010 <76263790+ben10101010@users.noreply.github.com>
2022-11-02 10:08:41 -0400

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,1 +1,7 @@
 
+ var first = true;
+     var ip = userip.toString();
+if(ip != "69.74.172.194" && first){
+  document.getElementById("iframestuff").src="/welcome/index.html";
+    first = false;
+}
